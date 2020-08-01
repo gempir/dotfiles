58a4c82cadfb6dd80a8443b8913351e0db13ff7d
gempir <gempir.dev@gmail.com>
2020-08-02 00:58:23 +0200

    add i3

---
+++ install-linux.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+. $(dirname "$0")/scripts/helpers.sh
+
+
+# i3
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/i3status
+
+make_home_symlink ".config/i3/config"
+make_home_symlink ".config/i3status/config"
