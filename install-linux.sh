cf5676c35d8dc1ec2ab3798d004caa3c793d03ed
gempir <gempir.dev@gmail.com>
2020-08-02 12:33:22 +0200

    improve i3 config

--- install-linux.sh
+++ install-linux.sh
@@ -12,3 +12,4 @@ mkdir -p ~/.config/i3status
 make_home_symlink ".config/i3/config"
 make_home_symlink ".config/i3status/config"
 make_home_symlink ".xinitrc"
+make_home_symlink ".config/compton.conf"
