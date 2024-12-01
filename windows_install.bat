6ad48b4177bd32d0ac71f2e8015b1eda4e7807d4
gempir <daniel.pasch.s@gmail.com>
2024-12-01 15:12:20 +0100

    new windows

--- windows_install.bat
+++ windows_install.bat
@@ -1,2 +1,4 @@
-mklink  C:\Users\danie\AppData\Roaming\Chatterino2\Settings\commands.json  C:\Users\danie\dotfiles\.local\share\chatterino\Settings\commands.json
-mklink  C:\Users\danie\AppData\Roaming\Chatterino2\Settings\window-layout.json  C:\Users\danie\dotfiles\.local\share\chatterino\Settings\window-layout.json
+del C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\commands.json
+del C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\window-layout.json
+mklink C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\commands.json C:\Users\gempir\dotfiles\.local\share\chatterino\Settings\commands.json
+mklink C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\window-layout.json C:\Users\gempir\dotfiles\.local\share\chatterino\Settings\window-layout.json
