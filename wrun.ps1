d722d33447e11ca8fb2fe5d2ee166cc25539d1a2
gempir <gempir.dev@gmail.com>
2025-04-13 09:26:06 +0200

    powershell

--- wrun.ps1
+++ wrun.ps1
@@ -1,4 +1,7 @@
-del C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\commands.json
-del C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\window-layout.json
-mklink C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\commands.json C:\Users\gempir\dotfiles\files\chatterino\commands.json
-mklink C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\window-layout.json C:\Users\gempir\dotfiles\files\chatterino\window-layout.json
+# Remove existing files
+Remove-Item -Path "C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\commands.json" -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\window-layout.json" -Force -ErrorAction SilentlyContinue
+
+# Create symbolic links
+New-Item -ItemType SymbolicLink -Path "C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\commands.json" -Target "C:\Users\gempir\dotfiles\files\chatterino\commands.json" -Force
+New-Item -ItemType SymbolicLink -Path "C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\window-layout.json" -Target "C:\Users\gempir\dotfiles\files\chatterino\window-layout.json" -Force
