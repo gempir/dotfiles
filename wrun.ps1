a42c09d445ce5cf75bfa8995293bf762ccd7016d
gempir <gempir.dev@gmail.com>
2025-04-13 09:27:37 +0200

    admin

--- wrun.ps1
+++ wrun.ps1
@@ -1,3 +1,24 @@
+if (!
+    #current role
+    (New-Object Security.Principal.WindowsPrincipal(
+        [Security.Principal.WindowsIdentity]::GetCurrent()
+    #is admin?
+    )).IsInRole(
+        [Security.Principal.WindowsBuiltInRole]::Administrator
+    )
+) {
+    #elevate script and exit current non-elevated runtime
+    Start-Process `
+        -FilePath 'powershell' `
+        -ArgumentList (
+            #flatten to single array
+            '-File', $MyInvocation.MyCommand.Source, $args `
+            | %{ $_ }
+        ) `
+        -Verb RunAs
+    exit
+}
+
 # Remove existing files
 Remove-Item -Path "C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\commands.json" -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\window-layout.json" -Force -ErrorAction SilentlyContinue
