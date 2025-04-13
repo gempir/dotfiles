2a75f65abb580bc0d987e801462c493a99251c78
gempir <gempir.dev@gmail.com>
2025-04-13 09:24:47 +0200

    wrun

---
+++ wrun.ps1
@@ -0,0 +1,4 @@
+del C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\commands.json
+del C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\window-layout.json
+mklink C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\commands.json C:\Users\gempir\dotfiles\files\chatterino\commands.json
+mklink C:\Users\gempir\AppData\Roaming\Chatterino2\Settings\window-layout.json C:\Users\gempir\dotfiles\files\chatterino\window-layout.json
